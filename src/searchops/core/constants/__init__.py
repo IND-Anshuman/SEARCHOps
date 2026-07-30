4312f01aa64c8e65ac3691c8b9c7be71758c0bf9
IND-Anshuman <anshu.2005.12@gmail.com>
2026-07-31 03:54:09 +0530

    core: add constants initializer

---
+++ src/searchops/core/constants/__init__.py
@@ -0,0 +1,19 @@
+"""System-wide constants."""
+
+from searchops.core.constants.system import (
+    DEFAULT_ENCODING,
+    DEFAULT_PAGE_SIZE,
+    DEFAULT_TIMEOUT_SECONDS,
+    MAX_RETRIES,
+    MAX_CONTENT_LENGTH,
+    PLATFORM_VERSION,
+)
+
+__all__ = [
+    "DEFAULT_ENCODING",
+    "DEFAULT_PAGE_SIZE",
+    "DEFAULT_TIMEOUT_SECONDS",
+    "MAX_RETRIES",
+    "MAX_CONTENT_LENGTH",
+    "PLATFORM_VERSION",
+]
