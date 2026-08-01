4ee38c948e0494279a8c1e0387ff3b0c3364fcd6
IND-Anshuman <anshu.2005.12@gmail.com>
2026-08-01 19:23:02 +0530

    application: initialize application package

---
+++ src/searchops/application/__init__.py
@@ -0,0 +1,5 @@
+"""Application layer package exports."""
+
+from searchops.application.research_service import ResearchApplicationService, ResearchJobStatus
+
+__all__ = ["ResearchApplicationService", "ResearchJobStatus"]
